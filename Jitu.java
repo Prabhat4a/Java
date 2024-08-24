582028e225a5a9fbcbb579767e3b9748695317c7
Prabhat4a <168079196+Prabhat4a@users.noreply.github.com>
2024-08-24 07:58:15 +0530

    Create Jitu.java

---
+++ Jitu.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class input {
+    public static void main(String[] arg) {
+        Scanner js = new Scanner(System.in);
+
+        //for a word
+        String name = js.next();
+        System.out.println(name);
+    }
+}
